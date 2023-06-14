6127033160e5d90fbafeba751c5bb19000694fa5
Surya Vivek <51923160+vivek-cyber@users.noreply.github.com>
2023-06-14 22:08:56 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+let p=document.createElement("print")
+window.addEventListener("DOMContentLoaded", (event) => {
+	p.innerText="DOM load success"
+  console.log("DOM fully loaded and parsed");
+});
